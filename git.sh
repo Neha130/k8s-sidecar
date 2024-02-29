82566299780bcaa8f04345f4f32b4c08e480da5a
Neha Sharma <156081591+Neha130@users.noreply.github.com>
2024-02-29 12:12:07 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -9,7 +9,7 @@ function clone_or_pull_repository {
         echo "Cloning the repository for the first time..."
         ls                
         cd "$local_path" || { echo "Directory not found"; exit 1; }
-        git clone "$repo_url" "$local_path" || { echo "Clone failed"; exit 1; }
+        git clone "$repo_url" . || { echo "Clone failed"; exit 1; }
 }
 send_discord_alert() {                                                           
     error_message="Failed to reload Grafana dashboards. HTTP Status code: $reload_response"
