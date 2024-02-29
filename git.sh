3233b9104a9326927cadf53ed7bce8c19921d401
Neha Sharma <156081591+Neha130@users.noreply.github.com>
2024-02-29 12:07:33 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -7,7 +7,6 @@ discord_webhook_url="$discord_webhook"
 function clone_or_pull_repository {
         # If the directory doesn't exist, clone the repository
         echo "Cloning the repository for the first time..."
-        mkdir -p "$local_path" || { echo "Failed to create directory"; exit 1; }
         git clone "$repo_url" "$local_path" || { echo "Clone failed"; exit 1; }
         cd "$local_path" || { echo "Directory not found"; exit 1; }
 }
